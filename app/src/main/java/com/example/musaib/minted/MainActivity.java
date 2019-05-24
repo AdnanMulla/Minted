de01c124150b51e77fbc968a00fe924c8b416f48
AdnanMulla <adnanmulla57@gmail.com>
2019-05-24 12:32:48 +0530

    Commit 2

--- app/src/main/java/com/example/musaib/minted/MainActivity.java
+++ app/src/main/java/com/example/musaib/minted/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.musaib.minted;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
